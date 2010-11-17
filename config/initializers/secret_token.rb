b89039c6629636c967a8a4517403b75e7ec6fd84
Andrew Peterson <ndp@mac.com>
2010-11-16 20:06:12 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uxspoke::Application.config.secret_token = 'd7a32b328de1f863fb0dd4dd7c7ef1c06f244a838e6012cfbcadf5ea2f6e836b0aaf22009c006f4351eac7b4870e609b1f709abc7d1f7b50238b0f877de5dc0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Uxspoke::Application.config.session_store :cookie_store, :key => '_uxspoke_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Uxspoke::Application.config.session_store :active_record_store
